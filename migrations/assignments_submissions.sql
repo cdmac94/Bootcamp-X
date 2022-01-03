c10af2c2b174e02533d75eab5305eb97d539b867
cdmac94 <collinmacleod79@gmail.com>
2022-01-03 23:01:30 +0000

    Add data for assignments and submissions

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE assignments (
-  id SERIAL PRIMARY KEY INTEGER NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content VARCHAR(400),
   day INTEGER,
@@ -8,7 +8,7 @@ CREATE TABLE assignments (
   );
 
   CREATE TABLE assignment_submissions (
-    id SERIAL PRIMARY KEY INTEGER NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
     student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
     duration INTEGER,
