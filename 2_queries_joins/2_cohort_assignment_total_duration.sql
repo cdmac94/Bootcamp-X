76da1353d75133f3fb0b5782e906939bbefd6b90
cdmac94 <collinmacleod79@gmail.com>
2022-01-04 22:21:29 +0000

    Add teachers and assitance requests tables + data

--- 2_queries_joins/2_cohort_assignment_total_duration.sql
+++ 2_queries_joins/2_cohort_assignment_total_duration.sql
@@ -1,5 +1,5 @@
 SELECT  SUM(assignments_submission.duration) AS total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
-JOIN cohorts ON cohort_id = cohort_id
+JOIN cohorts ON cohort.id = cohort_id
 WHERE cohort_name = 'FEB12'; 
